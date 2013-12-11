ff58a2df846d86166960c2c6f4f720d474feeac5
Streetfbmv <zach.j.email@gmail.com>
2013-12-11 18:15:03 -0500

    Here are my final changes for Sound Santa
    Project 3

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,9 +1,9 @@
+//Zach Jones created this on 11/15/13
 //self-executing anonymous function!
 (function(){
 
     //class-like object
     var Jukebox= function(playlistRef, positionRef, SC){
-        
         //store a reference to this object
         var me= this;
         
@@ -14,54 +14,50 @@
         this.sc= SC;
         
         //respond to a click on any list item in playlist   
-        this.playlist.on("click", "span", function(event) {
-            
+        this.playlist.on("click", "p", function(event) {
             var clickedElement = $(event.target);
             
             //update what is currently playing
             me.currentlyPlayingIndex = clickedElement.index();
             
             //get the associated track of the clicked element
-            var trackID = clickedElement.data("trackID");
-            
+            var trackID = clickedElement.data('index');
+			            
             //show the track title for "now playing"
             $("#txtNow").html(clickedElement.html());
             
             //play the selected song
             me.playSong(trackID);
         });
-   
     }
 
-    //Play next song
+    //play next song
     Jukebox.prototype.playNext=function(){
-         //try to play next song
-                this.currentlyPlayingIndex ++;
-                
-                //make sure there is a next song to play
-                if(this.currentlyPlayingIndex < this.playlist.children().length ) {
-                    var nextElement = this.playlist.find('li').eq(this.currentlyPlayingIndex);
-                    
-                    //update the now playing HTML
-                    $("#txtNow").html(nextElement.html() );
-    
-                    //play the next song
-                    this.playSong(nextElement.data("trackID"));
-    
-    
-    }
+        //try to play next song
+		this.currentlyPlayingIndex ++;
+		
+		//make sure there is a next song to play
+		if(this.currentlyPlayingIndex < $("#playlist div").size() ) {			
+			var nextElement = this.playlist.find('div p').eq(this.currentlyPlayingIndex);
+			
+			//update the now playing HTML
+			$("#txtNow").html(nextElement.html());
+			
+			//play the next song
+			this.playSong(nextElement.data("index"));
+        }
     }
     
     //play a song
     Jukebox.prototype.playSong=function playSong(trackID) {
         
         //store a reference to this object
-        var me= this;
+        var me = this;
     
-            //make sure this sound exists
+        //make sure this sound exists
         if(this.currentlyPlayingSound) {
             //if it does, stop it
-            this.currentlyPlayingSound.stop();
+            this.currentlyPlayingSound.stop(); 
         }
         
         SC.stream("/tracks/"+trackID, function(sound){
@@ -69,25 +65,25 @@
             me.currentlyPlayingSound = sound;
             
             sound.play({
-            whileplaying: function() {
-                me.position.val(this.position / this.duration);
-            },
-            onfinish: function() {
-               me.playNext();
-                
-            }
+                whileplaying: function() {
+                    me.position.val(this.position / this.duration);
+                },
+                onfinish: function() {
+                    me.playNext();
+                }
           });
         });
     }
     
     //stop currently playing song
     Jukebox.prototype.stop=function() {
+        
+        if(this.currentlyPlayingSound) {
+            this.currentlyPlayingSound.stop();
+			this.currentlyPlayingIndex = 0;
+        }
     
-    if(this.currentlyPlayingSound) {
-        this.currentlyPlayingSound.stop();
-    } }
-    //play next song
-    
+    }
     
     //export jukebox fro everybody to use
     window.Jukebox= Jukebox;
--- js/site.js
+++ js/site.js
@@ -1,9 +1,22 @@
+//Zach Jones created this on 11/15/13
 var trackListing = $("#content");
-var playlist = $("#playlist");
+var playlist = $("#playlist ul");
 var position = $("#position");
 var result;
 var currentlyPlayingSound;
 var currentlyPlayingIndex;
+var intRandTilt
+var intPlaylist = 0;
+
+homePage();
+
+function generateRand(){
+	intRandTilt = Math.floor((Math.random()*40)-20);
+}
+
+function homePage(){
+	$("#content").html("<h2>Home</h2><p>Talk to the reindeer!</p>");
+}
 
 SC.initialize({
     client_id: "3a0223a4404c4efe6133f785bc3cea54",
@@ -11,36 +24,53 @@ SC.initialize({
 });
 
 $("#home").click(function(){
-
-    $("#content").html("this is da home page :p ");
-
-})
+	$("#content").css("background", "none");
+	$("#pullBar").css({"-webkit-animation":"pullBarReverse 5s", "margin-left": "-88%"});
+	homePage();
+});
 
 $("#search").click(function(){
-
-    $("#content").html('<input id="txtSearch" /> ');
-
+	$("#txtSearch").focus();
+    $("#pullBar").css({"-webkit-animation":"pullBar 5s", "margin-left": "0px"});
 })
+
+$("#playlist").on("click", ".removeButton", function() {
+	var intClick = $(this).attr("id");
+	$("." + intClick).parent().remove();
+});
 
 //make a new Jukebox
 var jukebox= new Jukebox("#playlist", "#position", SC);
 
 $("#btnStop").click(function() {
-   jukebox.stop();
+	jukebox.stop();
+})
+
+$("#btnPlay").click(function() {
+    //get the associated track of the first element
+    var firstElement = $("#playlist p:first-child");
+    
+    var trackID = firstElement.data("index");
+    
+    $("#txtNow").html(firstElement.html());
+    
+    jukebox.playSong(trackID);
 })
 
 //when the search button is clicked...
-$("#btnSearch").click(function() {
+$("#btnSearch").click(function(){
     //get the search value
     var searchValue = $("#txtSearch").val();
-        
+    
+    $("#content").css("background-image", "url(assets/wishlist.png)");
+	  
     //list the songs of that value
     listSongs(searchValue);
 });
 
 //respond to a click on a list item in track listing
 //for ANY LIST ITEM regardless of when it was added
-$("#content").on("click", "li", function(event) { 
+trackListing.on("click", ".addToPlaylist", function(event) { 
     var clickedElement = $(event.target);
     
     //get index of the index of the data associated with clicked element
@@ -55,28 +85,46 @@ $("#content").on("click", "li", function(event) {
     //add some extra data to the cloned element
     //in specific, the trackID to ply whenever this item
     //is clicked
-    clonedElement.data("trackID", associatedData.id );
-    
-    //add the track title to our playlist div
-    playlist.append( clonedElement );
-    
+    clonedElement.data("trackID", associatedData.id);
+	    
+    intPlaylist = parseInt(intPlaylist);
+	
+	var trackID = clonedElement.data("trackID");
+	    
+    SC.get("/tracks/" + trackID, function(track){
+        //add the track title to our playlist div
+        playlist.append( clonedElement );
+        
+        var newPlaylistItem = "<div class='" + intPlaylist + "'>"
+                            + "<p data-index='" + trackID + "'>" + track.title + "</p>"
+                            + "<button id='" + intPlaylist + "' class='removeButton'>Remove</button></div>";
+							        
+        clonedElement.html(newPlaylistItem);
+		
+		generateRand();
+		
+		clonedElement.css({"transform":"rotate(" + intRandTilt + "deg)",
+							"-ms-transform":"rotate(" + intRandTilt + "deg)",
+							"-webkit-transform":"rotate(" + intRandTilt + "deg)"});
+        
+        intPlaylist++;
+    }); 
 });
 
 //lists songs for us
-function listSongs(query) {    
+function listSongs(query) {
     SC.get("/tracks/", { limit: 10, q: query }, function(tracks) {
         //store result for later
         result = tracks;
-        
-        //clear out track listing
-        trackListing.empty();
+		
+		$("#content").html("<h2>Search Results</h2>");
         
         //populate new values
         for(var i in tracks) {
             var curTrack = tracks[i];
             
             trackListing.append(
-                "<li>" + curTrack.title + "<span data-index='" + i + "'>Add to Playlist</span></li>"
+                "<li><span>" + curTrack.title + "</span><span data-index='" + i + "' class='addToPlaylist'>Add to Playlist</span></li>"
             );
         }
     });
